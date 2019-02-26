2cedf42c2c44efbe738910436c2a7c38492f05e3
1000ch <shogo.sensui@gmail.com>
2019-02-26 15:27:40 +0900

    Initial commit

---
+++ cli.js
@@ -0,0 +1,44 @@
+const package = require('./package');
+const minimist = require('minimist');
+const execa = require('execa');
+const Listr = require('listr');
+const inquirer = require('inquirer');
+
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    v: 'version'
+  }
+});
+
+if (argv.v || argv.version) {
+  console.log(package.version);
+  return;
+}
+
+const versions = ['major', 'minor', 'patch'];
+
+(async () => {
+  const arg = argv._[0];
+  const { version } = versions.includes(arg) ? arg : await inquirer.prompt([{
+    type: 'list',
+    name: 'version',
+    choices: versions
+  }]);
+
+  const tasks = new Listr([
+    {
+      title: 'Bumping version',
+      task: async () => {
+        await execa('npm', ['version', version])
+      }
+    },
+    {
+      title: 'Pushing tags',
+      task: async () => {
+        await execa('git', ['push', '--tags']);
+      }
+    }
+  ]);
+
+  await tasks.run();
+})();
---
+++ readme.md
@@ -0,0 +1,19 @@
+# versy
+
+Better versioning and tagging using `npm version`.
+
+## Install
+
+```bash
+$ npm install --save-dev versy
+```
+
+## Usage
+
+```bash
+$ versy
+```
+
+## License
+
+[MIT](https://1000ch.mit-license.org) © [Shogo Sensui](https://github.com/1000ch)
